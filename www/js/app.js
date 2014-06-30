2d2b01873a07c6d34a1a5146e524fef011063913
Ilya Medvedev <admin@iam-medvedev.ru>
2014-06-30 19:12:52 +0700

    Тест кроликов
    повесил кнопку на тест кроликов

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
-
+	
+	// demo
+	var i = 0;
+	$(".test-rabbit").click(function(){
+		if(i == 0) {
+			$(".rabbit").addClass("rabbit-show"); // показывает кролика
+			i = 1;
+		} else {
+			$(".rabbit").removeClass("rabbit-show"); // скрывает кролика
+			i = 0;	
+		}
+		
+		return false;
+	});
+	//
+	
 });
