7016fc15d9f085aaccc09e0c0dc6aa6c62f3e64a
Ilya Medvedev <admin@iam-medvedev.ru>
2014-06-30 20:45:53 +0700

    JS
    + таймер
    + конфиги

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function() {
+	var timer;
 	
+	// конфиг: монеты, выбранное оружие по id (0 - дефолтная морковка)
+	function config() {
+		if(typeof localStorage["coins"] == 'undefined') {
+			 localStorage["coins"] = 0;
+		}
+		
+		if(typeof localStorage["gun"] == 'undefined') {
+			 localStorage["gun"] = 0;
+		}
+	}
+	
+	function timer() {
+		timer = setInterval(function(){
+			if(localStorage["time"] >= 60) {
+				var minutes = Math.floor(localStorage["time"] / 60);
+				$(".minutes").text(minutes);
+				var seconds = localStorage["time"] - minutes * 60;
+				$(".seconds").text(seconds);
+			} else if(localStorage["time"] > 0) {
+				$(".minutes").text("0");
+				
+				if(localStorage["time"] >= 10) {
+					$(".seconds").text(localStorage["time"]);
+				} else {
+					$(".seconds").text("0"+localStorage["time"]);
+				}
+			} else {
+				$(".seconds").text("00");
+				stop();
+			}
+			
+			localStorage["time"]--;
+		}, 1000);
+	}
+	
+	function stop() {
+		clearInterval(timer);
+		alert("Конец");
+	}
+	
+	function init() {
+		config();
+		
+		localStorage["time"] = 11;
+		
+		timer();
+		
+		$(".coins").text(localStorage["coins"]);
+		
+		// сюда запилить функцию выпрыгивания кроликов
+	}
+	
+	init();
+		
 	// demo
 	var i = 0;
 	$(".test-rabbit").click(function(){
