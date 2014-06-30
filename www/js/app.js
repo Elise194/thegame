301972e86815aa17a1c6b0b60d6c4753d85658f1
Ilya Medvedev <admin@iam-medvedev.ru>
2014-06-30 19:14:17 +0700

    Тест кроликов 2
    маленький фикс

--- www/js/app.js
+++ www/js/app.js
@@ -5,9 +5,11 @@ $(document).ready(function(){
 	$(".test-rabbit").click(function(){
 		if(i == 0) {
 			$(".rabbit").addClass("rabbit-show"); // показывает кролика
+			$(this).text("Скрыть");
 			i = 1;
 		} else {
 			$(".rabbit").removeClass("rabbit-show"); // скрывает кролика
+			$(this).text("Показать");
 			i = 0;	
 		}
 		
