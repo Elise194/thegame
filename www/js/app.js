f8733600042d06b663037d80eda07e90ff13585b
Ilya Medvedev <admin@iam-medvedev.ru>
2014-07-01 11:12:57 +0700

    show_rabbits()
    - demo

--- www/js/app.js
+++ www/js/app.js
@@ -69,23 +69,4 @@ $(document).ready(function() {
 	}
 	
 	init();
-
-	// demo
-	/*
-	var i = 0;
-	$(".test-rabbit").click(function(){
-		if(i == 0) {
-			$(".rabbit").addClass("rabbit-show"); // показывает кролика
-			$(this).text("Скрыть");
-			i = 1;
-		} else {
-			$(".rabbit").removeClass("rabbit-show"); // скрывает кролика
-			$(this).text("Показать");
-			i = 0;	
-		}
-		
-		return false;
-	});
-	//
-	*/
 });
